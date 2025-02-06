82815d5ce259a03942c3dac46741342bf4a56f84
avery.white <averydouglascwhite@usf.edu>
2025-02-06 12:45:01 -0500

    Task 1 - Object Properties

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,11 @@
 console.log(`cc_5.s has succesfully loaded!`);
+
+// Task 1 - Object Properties
+let customer = {
+        name: `Juan John`,
+        age: 35,
+        email: `JuanJohn@surfshop.com`
+    };
+console.log (`Hello ${customer.name}, you are ${customer.age} years old and your email is ${customer.email}`);
+// Task 1 - End
+
